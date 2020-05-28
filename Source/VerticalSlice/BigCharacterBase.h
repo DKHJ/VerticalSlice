1f614fbf5baa2a91161caa8fab0d43f6975eeb1f
DKHJ <39827759+DKHJ@users.noreply.github.com>
2020-05-29 05:55:04 +1200

    Day 1 changes.
    Spawning Players
    Possessing Players
    Magic Platforms
    Runes
    Interactions
    Camera Smoothing
    Player Turn Rates

---
+++ Source/VerticalSlice/BigCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VerticalSliceCharacter.h"
+#include "BigCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VERTICALSLICE_API ABigCharacterBase : public AVerticalSliceCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/VerticalSlice/MagicPlatform.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MagicPlatform.h"
+
+// Sets default values
+AMagicPlatform::AMagicPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMagicPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMagicPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
