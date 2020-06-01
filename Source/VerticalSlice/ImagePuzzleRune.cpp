0472836080c8164e82da16a3f603c376320a87f0
DKHJ <39827759+DKHJ@users.noreply.github.com>
2020-06-02 10:27:56 +1200

    thought i comitted this earlier

---
+++ Source/VerticalSlice/ImagePuzzleRune.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ImagePuzzleRune.h"
+
+// Sets default values
+AImagePuzzleRune::AImagePuzzleRune()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AImagePuzzleRune::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AImagePuzzleRune::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
