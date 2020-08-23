10ae75e029588bc45ebeef901a2f9696c73b339c
JSotoBun <BunSoto88@gmail.com>
2020-08-23 20:17:03 +1200

    Trying to re do summer puzzle

---
+++ Source/VerticalSlice/SummerPushPuzzle.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SummerPushPuzzle.h"
+
+// Sets default values
+ASummerPushPuzzle::ASummerPushPuzzle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASummerPushPuzzle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASummerPushPuzzle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
